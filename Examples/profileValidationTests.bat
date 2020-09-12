c98cc859a7c817b86e162a1334647427528883e4
kevinmayfieldAire <KevinMayfield@users.noreply.github.com>
2020-09-12 10:02:53 +0100

    CarePlan example

---
+++ Examples/profileValidationTests.bat
@@ -0,0 +1,5 @@
+
+
+java -jar org.hl7.fhir.validator.jar FD-Bundle-Example-3.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.Spine.r4/-/UK.Spine.r4-0.0.10-dev.tgz
+
+
